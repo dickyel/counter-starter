0e3a702bbce6bc4c1fec96381cbc18e86a297c1e
Dell <muhammad.dicky7@gmail.com>
2024-01-20 15:50:45 +0700

    event handling

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import './App.css';
-import shopIcon from './assets/shopping-icon.svg';
+import "./App.css";
+import shopIcon from "./assets/shopping-icon.svg";
 
 function App() {
+  const [value, setValue] = useState('');
+
+  console.log(value);
 
   return (
     // react fragment
     <>
-
       {/* 
        ini adalah bagian navbar 
       */}
-      <nav className='nav'>
-        <img className='nav-img' src={shopIcon} alt='shop-icon' />
-        <h1 className='nav-title'>List Belanjaan</h1>
-
+      <nav className="nav">
+        <img className="nav-img" src={shopIcon} alt="shop-icon" />
+        <h1 className="nav-title">List Belanjaan</h1>
       </nav>
 
-      <section className='container'>
-        <form className='form-belanja'>
-          <input 
-          className='input-belanja'
-          type='text'
-          placeholder='buat list belanjaan'
+      {/* section untuk bagian form belanja */}
+
+      <section className="container">
+        <form className="form-belanja">
+          <input onChange={ (e) => { setValue(e.target.value)}}
+            value={value}
+            className="input-belanja"
+            type="text"
+            placeholder="buat list belanjaan"
           />
-          <button type='submit' className='tombol-tambah'>Tambah</button>
+          <button type="submit" className="tombol-tambah">
+            Tambah
+          </button>
         </form>
       </section>
-    </>    
+    </>
   );
 }
 
