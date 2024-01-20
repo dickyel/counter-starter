ca6b88c35c8870583ac3f6161c108a4039ce7678
Dell <muhammad.dicky7@gmail.com>
2024-01-20 15:28:44 +0700

    counter starter form input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import { useState } from 'react';
 
 import './App.css';
+import shopIcon from './assets/shopping-icon.svg';
 
 function App() {
 
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <h1>{count}</h1>
+    // react fragment
+    <>
 
-      <button onClick={ 
-        () => {setCount(count + 1)} }> 
-        + (PLUS)
-      </button>
-      <button
-      onClick={() => {setCount(count - 1)}}> 
-        - (KURANG)
-      </button>
-    </div>
+      {/* 
+       ini adalah bagian navbar 
+      */}
+      <nav className='nav'>
+        <img className='nav-img' src={shopIcon} alt='shop-icon' />
+        <h1 className='nav-title'>List Belanjaan</h1>
+
+      </nav>
+
+      <section className='container'>
+        <form className='form-belanja'>
+          <input 
+          className='input-belanja'
+          type='text'
+          placeholder='buat list belanjaan'
+          />
+          <button type='submit' className='tombol-tambah'>Tambah</button>
+        </form>
+      </section>
+    </>    
   );
 }
 
