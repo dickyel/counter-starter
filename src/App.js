731794853ec734bf945190140496c3a149b896d1
Dell <muhammad.dicky7@gmail.com>
2024-01-20 14:43:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+
+      <button onClick={ 
+        () => {setCount(count + 1)} }> 
+        + (PLUS)
+      </button>
+      <button
+      onClick={() => {setCount(count - 1)}}> 
+        - (KURANG)
+      </button>
+    </div>
+  );
+}
+
+export default App;
